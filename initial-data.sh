3dd76a0510f1275da6bb88d8a1a45ed0ee76095f
Jonathan Meyer <jon@gisjedi.com>
2017-05-10 21:48:09 -0400

    Fix following repo rename

--- initial-data.sh
+++ initial-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec dockercomposemodis_postgis_1 apk add -U curl unzip ca-certificates openssl
-docker exec -u postgres dockercomposemodis_postgis_1 sh /tmp/postgis/initial-data.sh 
+docker exec dockercomposefeatures_postgis_1 apk add -U curl unzip ca-certificates openssl
+docker exec -u postgres dockercomposefeatures_postgis_1 sh /tmp/postgis/initial-data.sh 
 
-docker exec dockercomposemodis_geoserver_1 apk add -U curl
-docker exec dockercomposemodis_geoserver_1 sh /tmp/geoserver/initial-data.sh 
+docker exec dockercomposefeatures_geoserver_1 apk add -U curl
+docker exec dockercomposefeatures_geoserver_1 sh /tmp/geoserver/initial-data.sh 
